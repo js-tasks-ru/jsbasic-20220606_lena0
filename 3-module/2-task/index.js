920d9d24d42ca47dd395e4b31b215988fb9136f6
Lena <lena.pokrovskaya@yahoo.com>
2022-06-29 15:04:11 +0300

    3 module 2 task

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,3 +1,11 @@
 function filterRange(arr, a, b) {
-  // ваш код...
+  let newArr = [];
+  arr.filter(function(item, index, array) {
+    if (item >= a && item <= b) {
+      newArr.push(item);
+    } else {
+      return [];
+    }
+  });
+  return newArr;
 }
