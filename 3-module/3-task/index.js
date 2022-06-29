d565cbaabff082ca705482e288a435783068445e
Lena <lena.pokrovskaya@yahoo.com>
2022-06-29 15:08:43 +0300

    3 module 3 task

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,13 @@
 function camelize(str) {
-  // ваш код...
+  let arr = str.split("");
+  let result;
+  arr.forEach(function(item, index, array) {
+    let indexOfHyphen = arr.indexOf('-');
+    let indexOfCapitalLetter = indexOfHyphen + 1;
+    let indexOfFirstLetter = 0;
+    arr[indexOfHyphen] = '';
+    arr[indexOfCapitalLetter] = arr[indexOfCapitalLetter].toUpperCase();
+    arr[indexOfFirstLetter] = arr[indexOfFirstLetter].toLowerCase();
+  });
+  return result = arr.join("");
 }
