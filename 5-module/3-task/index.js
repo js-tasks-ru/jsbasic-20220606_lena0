53bf61694de0e203d8a64c22ccb29c21a8ca7cad
Lena <lena.pokrovskaya@yahoo.com>
2022-07-03 16:55:18 +0300

    5 module 3 task

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,4 +1,30 @@
 function initCarousel() {
+  let carousel = document.querySelector('.carousel');
+  let carouselInner = carousel.querySelector('.carousel__inner');
+  let arrOfSlides = Array.from(carouselInner.children);
+  let carouselSlide = carousel.querySelector('.carousel__slide');
+  let carouselSlideWidth = carouselSlide.offsetWidth;
+  let arrowRight = carousel.querySelector('.carousel__arrow_right');
+  let arrowLeft = carousel.querySelector('.carousel__arrow_left');
+  let slideIndex = 0;
+  let maxSlides = 3;
 
+  arrowLeft.style.display = 'none';
+  arrowRight.addEventListener('click', () => {
+    arrowLeft.style.display = '';
+    arrOfSlides[slideIndex++];
+    carouselInner.style.transform = `translateX(-${carouselSlideWidth * slideIndex}px)`;
+    if (slideIndex > (maxSlides - 1)) {
+      arrowRight.style.display = 'none';
+    }
+  });
 
+  arrowLeft.addEventListener('click', () => {
+    arrOfSlides[slideIndex--];
+    carouselInner.style.transform = `translateX(-${carouselSlideWidth * slideIndex}px)`;
+    if (slideIndex === 0) {
+      arrowLeft.style.display = 'none';
+      arrowRight.style.display = '';
+    }
+  });
 }
