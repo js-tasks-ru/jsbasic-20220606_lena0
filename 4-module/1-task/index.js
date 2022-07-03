440a55a8a1ee0fd066b39c8906580f5326fc6748
Lena <lena.pokrovskaya@yahoo.com>
2022-07-03 10:58:12 +0300

    4 module 1, 2, 3 tasks

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,13 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let body = document.getElementsByTagName('body')[0];
+  let newUl = document.createElement('ul');
+  body.append(newUl);
+  let newLi = document.createElement('li');
+
+  for (let i = 0; i < friends.length; i++) {
+    newLi = document.createElement('li');
+    newLi.textContent = `${friends[i].firstName} ${friends[i].lastName}`;
+    newUl.append(newLi);
+  }
+  return newUl;
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,30 @@
 function highlight(table) {
-  // ваш код...
+  let tbodyCollection = table.getElementsByTagName("tbody");
+  let tbody = Array.from(tbodyCollection)[0];
+  let trs = tbody.rows;
+  let tds = tbody.querySelectorAll('.js-teachers td');
+  for (let i = 0; i < trs.length; i++) {
+    if (trs[i].lastElementChild.getAttribute("data-available") === 'true') {
+      trs[i].classList.add("available");
+    } else if (trs[i].lastElementChild.getAttribute("data-available") === 'false') {
+      trs[i].classList.add("unavailable");
+    } else {
+      trs[i].hidden = true;
+    }
+  }
+
+  for (let i = 0; i < trs.length; i++) {
+    if (trs[i].cells[2].innerHTML == 'm') {
+      trs[i].classList.add("male");
+    } else {
+      trs[i].classList.add("female");
+    }
+
+  }
+
+  for (let i = 0; i < trs.length; i++) {
+    if (trs[i].cells[1].innerHTML < 18) {
+      trs[i].style.textDecoration = "line-through";
+    }
+  }
 }
