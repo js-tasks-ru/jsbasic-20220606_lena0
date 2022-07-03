436cca0d985401c8085fe29602d9374413879934
Lena <lena.pokrovskaya@yahoo.com>
2022-07-03 14:15:20 +0300

    4 module 2 task(edits)

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -2,6 +2,6 @@ function toggleText() {
   let toggle = document.querySelector('.toggle-text-button');
   let text = document.querySelector('#text');
   toggle.addEventListener('click', () => {
-    text.toggleAttribute("hidden");
+    text.toggleAttribute('hidden');
   });
 }
